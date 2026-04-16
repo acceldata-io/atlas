6cec65ca0bc9b20dbf065e5a45dc5bf92196d5e7
Prabhjyot Singh <prabhjyotsingh@gmail.com>
2026-04-16 16:50:47 -0400

    ODP-6366: Update Jersey to 2.17.x

--- webapp/src/main/java/org/apache/atlas/web/util/AtlasJsonProvider.java
+++ webapp/src/main/java/org/apache/atlas/web/util/AtlasJsonProvider.java
@@ -22,42 +22,50 @@ import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.jaxrs.json.JacksonJaxbJsonProvider;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.Produces;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.MessageBodyReader;
+import javax.ws.rs.ext.MessageBodyWriter;
 import javax.ws.rs.ext.Provider;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Type;
 
 
 @Provider
 @Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 @Component
-public class AtlasJsonProvider extends JacksonJaxbJsonProvider {
+public class AtlasJsonProvider implements MessageBodyReader<Object>, MessageBodyWriter<Object> {
     private static final Logger LOG = LoggerFactory.getLogger(AtlasJsonProvider.class);
 
     private static final ObjectMapper mapper = new ObjectMapper()
                                                     .configure(DeserializationFeature.USE_BIG_DECIMAL_FOR_FLOATS, true);
 
     public AtlasJsonProvider() {
-        super(mapper, JacksonJaxbJsonProvider.DEFAULT_ANNOTATIONS);
-
         LOG.info("AtlasJsonProvider() instantiated");
+    }
+
+    @Override
+    public boolean isReadable(Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType) {
+        return mediaType != null && mediaType.isCompatible(MediaType.APPLICATION_JSON_TYPE)
+                && !String.class.isAssignableFrom(type);
     }
 
     @Override
     public Object readFrom(Class<Object> type, Type genericType, Annotation[] annotations, MediaType mediaType,
                            MultivaluedMap<String, String> httpHeaders, InputStream entityStream) throws IOException {
         try {
-            return super.readFrom(type, genericType, annotations, mediaType, httpHeaders, entityStream);
+            return mapper.readValue(entityStream, mapper.constructType(genericType));
         } catch (JsonParseException jpe) {
             LOG.error("Malformed json passed to server", jpe);
             throw new WebApplicationException(Servlets.getErrorResponse(jpe.getMessage(), Response.Status.BAD_REQUEST));
@@ -65,5 +73,23 @@ public class AtlasJsonProvider extends JacksonJaxbJsonProvider {
             LOG.error("Malformed json passed to server, incorrect data type used", jme);
             throw new WebApplicationException(Servlets.getErrorResponse(jme.getMessage(), Response.Status.BAD_REQUEST));
         }
+    }
+
+    @Override
+    public boolean isWriteable(Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType) {
+        return mediaType != null && mediaType.isCompatible(MediaType.APPLICATION_JSON_TYPE)
+                && !String.class.isAssignableFrom(type);
+    }
+
+    @Override
+    public long getSize(Object o, Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType) {
+        return -1;
+    }
+
+    @Override
+    public void writeTo(Object o, Class<?> type, Type genericType, Annotation[] annotations,
+                        MediaType mediaType, MultivaluedMap<String, Object> httpHeaders,
+                        OutputStream entityStream) throws IOException {
+        mapper.writeValue(entityStream, o);
     }
 }
